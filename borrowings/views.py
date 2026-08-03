be258f8f80b4c632cbf5a05f31573f08bb1f705b
Vitaliy Stepanyuk <vist98@gmail.com>
2026-08-04 00:00:41 +0300

    implement create borrowings func

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,12 +1,13 @@
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAuthenticated
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerializer
+from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerializer, BorrowingCreateSerializer
 
 
 class BorrowingViewSet(
     mixins.RetrieveModelMixin,
     mixins.ListModelMixin,
+    mixins.CreateModelMixin,
     viewsets.GenericViewSet
 ):
     queryset = Borrowing.objects.all()
@@ -16,5 +17,7 @@ class BorrowingViewSet(
     def get_serializer_class(self):
         if self.action == "retrieve":
             return BorrowingDetailSerializer
+        elif self.action == "create":
+            return BorrowingCreateSerializer
         else:
             return BorrowingSerializer
