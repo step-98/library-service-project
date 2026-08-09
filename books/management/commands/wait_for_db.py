5c112c0db50089c73180d80ff9ed7251753bd22e
Vitaliy Stepanyuk <vist98@gmail.com>
2026-08-09 22:04:43 +0300

    add .flake8 configurations

--- books/management/commands/wait_for_db.py
+++ books/management/commands/wait_for_db.py
@@ -10,16 +10,10 @@ class Command(BaseCommand):
         for _ in range(10):
             try:
                 connection.ensure_connection()
-                self.stdout.write(
-                    self.style.SUCCESS("Database connection established")
-                )
+                self.stdout.write(self.style.SUCCESS("Database connection established"))
                 break
             except OperationalError:
-                self.stdout.write(
-                    self.style.WARNING("Waiting for db connection 1 sec")
-                )
+                self.stdout.write(self.style.WARNING("Waiting for db connection 1 sec"))
                 sleep(1)
         else:
-            raise OperationalError(
-                "Could not connect to database after 10 attempts"
-            )
+            raise OperationalError("Could not connect to database after 10 attempts")
